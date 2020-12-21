713109186f75f88dfc2589e606629d2c15912ab8
Ignacio <rogerpeke97@gmail.com>
2020-12-21 12:45:39 -0300

    Boceto de la calculadora

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+let style = {
+  app: {
+
+  }
+}
 
 function App() {
+  const [input, setInput] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+       <div>
+         <input onChange={(e)=>{
+           isNaN(e.currentTarget.value) ? setInput(0) : setInput(e.currentTarget.value)
+           }} value={input} />
+       </div>
+       <div style={{display: "flex"}}>
+         <div>C</div>
+         <div>%</div>
+       </div>
+       <div style={{display: "flex"}}>
+         <div>7</div>
+         <div>8</div>
+         <div>9</div>
+         <div>X</div>
+       </div>
+       <div style={{display: "flex"}}>
+         <div>4</div>
+         <div>5</div>
+         <div>6</div>
+         <div>-</div>
+       </div> 
+       <div style={{display: "flex"}}>
+         <div>1</div>
+         <div>2</div>
+         <div>3</div>
+         <div>+</div>
+       </div>
+       <div style={{display: "flex"}}>
+         <div>0</div>
+         <div>=</div>
+       </div>
+     </div>
     </div>
   );
 }
